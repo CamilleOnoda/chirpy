2071fee8d19afb58ced9c1570da6a7fb7375eeb6
Camille Onoda <info@camilleonoda.com>
2026-03-30 16:50:53 +0900

    feat: add basic HTTP server with ServeMux on port 8080

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CamilleOnoda/chirpy
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	log.Printf("Server started on port: %v", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server error: %v", err)
+	}
+}
